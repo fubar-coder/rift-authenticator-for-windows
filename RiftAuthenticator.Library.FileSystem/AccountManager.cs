de94ff327b348989c997c6d8f057f363e4938320
fubar-dev <mark.junker75@googlemail.com>
2011-08-25 21:45:01 +0200

    New file system based back-end

---
+++ RiftAuthenticator.Library.FileSystem/AccountManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiftAuthenticator.Library.FileSystem
+{
+    public class AccountManager : Library.AccountManagerBase
+    {
+        private const string StoredAccountsKey = "stored_accounts";
+
+        private string GetGlobalSettingsFileName()
+        {
+            return System.IO.Path.Combine(Account.GetAccountFolder(), "settings.xml");
+        }
+
+        public override IAccount CreateAccount()
+        {
+            return new Account();
+        }
+
+        public override int StoredAccounts
+        {
+            get
+            {
+                var configFileName = GetGlobalSettingsFileName();
+                var map = Account.ReadMap(configFileName);
+                if (!map.ContainsKey(StoredAccountsKey))
+                    return 0;
+                return (int)map[StoredAccountsKey];
+            }
+            set
+            {
+                var configFileName = GetGlobalSettingsFileName();
+                var map = Account.ReadMap(configFileName);
+                map[StoredAccountsKey] = value;
+                Account.WriteMap(configFileName, map);
+            }
+        }
+    }
+}
