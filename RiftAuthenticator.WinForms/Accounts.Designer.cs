f6eeb92ef5e8034c5696193799c1d451e0f25ed5
fubar-dev <mark.junker75@googlemail.com>
2011-08-26 19:28:47 +0200

    German translation for the new accounts management dialog

--- RiftAuthenticator.WinForms/Accounts.Designer.cs
+++ RiftAuthenticator.WinForms/Accounts.Designer.cs
@@ -29,24 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accounts));
             this.AccountGrid = new System.Windows.Forms.DataGridView();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deviceIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.formattedSerialKeyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iAccountBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.ApplyAccountChanges = new System.Windows.Forms.Button();
             this.CancelAccountChanges = new System.Windows.Forms.Button();
             this.MoceAccountUp = new System.Windows.Forms.Button();
             this.MoveAccountDown = new System.Windows.Forms.Button();
-            this.iAccountBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.deviceIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.formattedSerialKeyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.AccountGrid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iAccountBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // AccountGrid
             // 
-            this.AccountGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.AccountGrid, "AccountGrid");
             this.AccountGrid.AutoGenerateColumns = false;
             this.AccountGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.AccountGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -54,91 +53,70 @@
             this.deviceIdDataGridViewTextBoxColumn,
             this.formattedSerialKeyDataGridViewTextBoxColumn});
             this.AccountGrid.DataSource = this.iAccountBindingSource;
-            this.AccountGrid.Location = new System.Drawing.Point(12, 12);
             this.AccountGrid.Name = "AccountGrid";
-            this.AccountGrid.Size = new System.Drawing.Size(357, 170);
-            this.AccountGrid.TabIndex = 0;
-            // 
-            // ApplyAccountChanges
-            // 
-            this.ApplyAccountChanges.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.ApplyAccountChanges.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.ApplyAccountChanges.Location = new System.Drawing.Point(12, 188);
-            this.ApplyAccountChanges.Name = "ApplyAccountChanges";
-            this.ApplyAccountChanges.Size = new System.Drawing.Size(75, 23);
-            this.ApplyAccountChanges.TabIndex = 1;
-            this.ApplyAccountChanges.Text = "OK";
-            this.ApplyAccountChanges.UseVisualStyleBackColor = true;
-            // 
-            // CancelAccountChanges
-            // 
-            this.CancelAccountChanges.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.CancelAccountChanges.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelAccountChanges.Location = new System.Drawing.Point(329, 188);
-            this.CancelAccountChanges.Name = "CancelAccountChanges";
-            this.CancelAccountChanges.Size = new System.Drawing.Size(75, 23);
-            this.CancelAccountChanges.TabIndex = 2;
-            this.CancelAccountChanges.Text = "Cancel";
-            this.CancelAccountChanges.UseVisualStyleBackColor = true;
-            // 
-            // MoceAccountUp
-            // 
-            this.MoceAccountUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.MoceAccountUp.Location = new System.Drawing.Point(375, 12);
-            this.MoceAccountUp.Name = "MoceAccountUp";
-            this.MoceAccountUp.Size = new System.Drawing.Size(29, 25);
-            this.MoceAccountUp.TabIndex = 3;
-            this.MoceAccountUp.Text = "↑";
-            this.MoceAccountUp.UseVisualStyleBackColor = true;
-            // 
-            // MoveAccountDown
-            // 
-            this.MoveAccountDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.MoveAccountDown.Location = new System.Drawing.Point(375, 157);
-            this.MoveAccountDown.Name = "MoveAccountDown";
-            this.MoveAccountDown.Size = new System.Drawing.Size(29, 25);
-            this.MoveAccountDown.TabIndex = 4;
-            this.MoveAccountDown.Text = "↓";
-            this.MoveAccountDown.UseVisualStyleBackColor = true;
-            // 
-            // iAccountBindingSource
-            // 
-            this.iAccountBindingSource.DataSource = typeof(RiftAuthenticator.Library.IAccount);
             // 
             // descriptionDataGridViewTextBoxColumn
             // 
             this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
-            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            resources.ApplyResources(this.descriptionDataGridViewTextBoxColumn, "descriptionDataGridViewTextBoxColumn");
             this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
             // 
             // deviceIdDataGridViewTextBoxColumn
             // 
             this.deviceIdDataGridViewTextBoxColumn.DataPropertyName = "DeviceId";
-            this.deviceIdDataGridViewTextBoxColumn.HeaderText = "Device ID";
+            resources.ApplyResources(this.deviceIdDataGridViewTextBoxColumn, "deviceIdDataGridViewTextBoxColumn");
             this.deviceIdDataGridViewTextBoxColumn.Name = "deviceIdDataGridViewTextBoxColumn";
             this.deviceIdDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // formattedSerialKeyDataGridViewTextBoxColumn
             // 
             this.formattedSerialKeyDataGridViewTextBoxColumn.DataPropertyName = "FormattedSerialKey";
-            this.formattedSerialKeyDataGridViewTextBoxColumn.HeaderText = "Serial Key";
+            resources.ApplyResources(this.formattedSerialKeyDataGridViewTextBoxColumn, "formattedSerialKeyDataGridViewTextBoxColumn");
             this.formattedSerialKeyDataGridViewTextBoxColumn.Name = "formattedSerialKeyDataGridViewTextBoxColumn";
             this.formattedSerialKeyDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // iAccountBindingSource
+            // 
+            this.iAccountBindingSource.DataSource = typeof(RiftAuthenticator.Library.IAccount);
+            // 
+            // ApplyAccountChanges
+            // 
+            resources.ApplyResources(this.ApplyAccountChanges, "ApplyAccountChanges");
+            this.ApplyAccountChanges.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ApplyAccountChanges.Name = "ApplyAccountChanges";
+            this.ApplyAccountChanges.UseVisualStyleBackColor = true;
+            // 
+            // CancelAccountChanges
+            // 
+            resources.ApplyResources(this.CancelAccountChanges, "CancelAccountChanges");
+            this.CancelAccountChanges.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelAccountChanges.Name = "CancelAccountChanges";
+            this.CancelAccountChanges.UseVisualStyleBackColor = true;
+            // 
+            // MoceAccountUp
+            // 
+            resources.ApplyResources(this.MoceAccountUp, "MoceAccountUp");
+            this.MoceAccountUp.Name = "MoceAccountUp";
+            this.MoceAccountUp.UseVisualStyleBackColor = true;
+            // 
+            // MoveAccountDown
+            // 
+            resources.ApplyResources(this.MoveAccountDown, "MoveAccountDown");
+            this.MoveAccountDown.Name = "MoveAccountDown";
+            this.MoveAccountDown.UseVisualStyleBackColor = true;
             // 
             // Accounts
             // 
             this.AcceptButton = this.ApplyAccountChanges;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelAccountChanges;
-            this.ClientSize = new System.Drawing.Size(416, 223);
             this.Controls.Add(this.MoveAccountDown);
             this.Controls.Add(this.MoceAccountUp);
             this.Controls.Add(this.CancelAccountChanges);
             this.Controls.Add(this.ApplyAccountChanges);
             this.Controls.Add(this.AccountGrid);
             this.Name = "Accounts";
-            this.Text = "Accounts";
             ((System.ComponentModel.ISupportInitialize)(this.AccountGrid)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iAccountBindingSource)).EndInit();
             this.ResumeLayout(false);
