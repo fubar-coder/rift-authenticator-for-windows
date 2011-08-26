d1d7d1746c5aa13887d7a49dd8b50d1530df66fc
fubar-dev <mark.junker75@googlemail.com>
2011-08-26 20:32:02 +0200

    Avoid direct mapping to the original account. Only copy the data to the original account when the account must be saved.

--- RiftAuthenticator.Library/AccountProxy.cs
+++ RiftAuthenticator.Library/AccountProxy.cs
@@ -4,99 +4,43 @@ using System.Text;
 
 namespace RiftAuthenticator.Library
 {
-    public class AccountProxy : Library.IAccount
+    public class AccountProxy : Library.AccountBase
     {
         public Library.IAccount OriginalAccount { get; private set; }
 
         public AccountProxy(Library.IAccount originalAccount)
         {
             OriginalAccount = originalAccount;
+            CopyDataFromOriginalAccount();
         }
 
-        public virtual string Description
+        private void CopyDataFromOriginalAccount()
         {
-            get
-            {
-                return OriginalAccount.Description;
-            }
-            set
-            {
-                OriginalAccount.Description = value;
-            }
+            this.Description = OriginalAccount.Description;
+            this.DeviceId = OriginalAccount.DeviceId;
+            this.SerialKey = OriginalAccount.SerialKey;
+            this.SecretKey = OriginalAccount.SecretKey;
+            this.TimeOffset = OriginalAccount.TimeOffset;
         }
 
-        public virtual string DeviceId
+        private void CopyDataToOriginalAccount()
         {
-            get
-            {
-                return OriginalAccount.DeviceId;
-            }
-            set
-            {
-                OriginalAccount.DeviceId = value;
-            }
+            OriginalAccount.Description = this.Description;
+            OriginalAccount.DeviceId = this.DeviceId;
+            OriginalAccount.SerialKey = this.SerialKey;
+            OriginalAccount.SecretKey = this.SecretKey;
+            OriginalAccount.TimeOffset = this.TimeOffset;
         }
 
-        public virtual string SerialKey
-        {
-            get
-            {
-                return OriginalAccount.SerialKey;
-            }
-            set
-            {
-                OriginalAccount.SerialKey = value;
-            }
-        }
-
-        public virtual string SecretKey
-        {
-            get
-            {
-                return OriginalAccount.SecretKey;
-            }
-            set
-            {
-                OriginalAccount.SecretKey = value;
-            }
-        }
-
-        public virtual long TimeOffset
-        {
-            get
-            {
-                return OriginalAccount.TimeOffset;
-            }
-            set
-            {
-                OriginalAccount.TimeOffset = value;
-            }
-        }
-
-        public virtual string FormattedSerialKey
-        {
-            get { return OriginalAccount.FormattedSerialKey; }
-        }
-
-        public virtual bool IsEmpty
-        {
-            get { return OriginalAccount.IsEmpty; }
-        }
-
-        public virtual Library.LoginToken CalculateToken()
-        {
-            if (OriginalAccount.IsEmpty)
-                throw new InvalidOperationException();
-            return OriginalAccount.CalculateToken();
-        }
-
-        public virtual void Load(Library.IAccountManager accountManager, int accountIndex)
+        public override void Load(IAccountManager accountManager, int accountIndex)
         {
             OriginalAccount.Load(accountManager, accountIndex);
+            CopyDataFromOriginalAccount();
         }
 
-        public virtual void Save(Library.IAccountManager accountManager, int accountIndex)
+        public override void Save(IAccountManager accountManager, int accountIndex)
         {
+            CopyDataToOriginalAccount();
             OriginalAccount.Save(accountManager, accountIndex);
         }
     }
