71fc1f0162625d51fec3aa54e01861415fe38233
fubar-dev <mark.junker75@googlemail.com>
2011-08-27 16:50:28 +0200

    Show window for first application start

---
+++ RiftAuthenticator.WinForms/FirstAppStart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RiftAuthenticator.WinForms
+{
+    public partial class FirstAppStart : Form
+    {
+        public FirstAppStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
